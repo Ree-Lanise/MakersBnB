a0f020a296460d71b725d19ffae35b2d57ed7267
Jess <jessicacktodd@gmail.com>
2024-03-05 17:10:02 +0000

    property html

---
+++ lib/list_of_spaces.py
@@ -0,0 +1,5 @@
+# name = {1: "Chestnut Eco Lodge Woodland Escape", 2: "The Hazel Hide"}
+# description = {1: "House with garden", 2: "Luxury Eco A-Frame Cabin"}
+# price = {1: "Price: £101/night", 2: "Price: 240/night"}
+# user_id = {1: "Owner: 1", 2: "Owner: 3"}
+# booked_status = {1: "Booked status: False", 2:"Booked status: False"}
