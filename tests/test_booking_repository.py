581bae6281ddaeecbcc17dbf44b0417de0aa5ef1
George Paul <george.paul5236@gmail.com>
2024-03-07 16:59:53 +0000

    fixed tests

--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -29,7 +29,7 @@ def test_create(db_connection):
     repo = BookingRepository(db_connection)
     new_booking = repo.create(Booking(None, 1, 2, 1, datetime.date(2023,4,1), datetime.date(2023,4,5), None, "greenhouse"))
     result = repo.find(new_booking.id)
-    assert result == Booking(6, 1, 2, 1, datetime.date(2023,4,1), datetime.date(2023,4,5), "pending", "greenhouse")
+    assert result == Booking(6, 1, 2, 1, datetime.date(2023,4,1), datetime.date(2023,4,5), "PENDING", "greenhouse")
 
 def test_delete(db_connection): 
     db_connection.seed('seeds/Users.sql')
@@ -38,7 +38,7 @@ def test_delete(db_connection):
     repo = BookingRepository(db_connection)
     booking_to_delete = Booking(None, 1, 2, 1, datetime.date(2023,4,1), datetime.date(2023,4,5), "pending", "greenhouse")
     repo.create(booking_to_delete)
-    repo.delete(booking_to_delete)
+    repo.delete(6)
     assert repo.all() == [Booking(1, 1, 5, 1, datetime.date(2023, 3, 1), datetime.date(2023,3,5), "pending",'Chestnut Eco Lodge Woodland Escape' ),
                         Booking(2, 2, 4, 2, datetime.date(2023,3,5), datetime.date(2023,3,8), "pending", 'The Hazel Hide'),
                         Booking(3, 3, 3, 3, datetime.date(2023,3,11), datetime.date(2023,3,15), "pending", 'Entire Contemporary Barn'),
